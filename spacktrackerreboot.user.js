75e63cff7c019640ca2a3b0140858134286a6388
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-04-08 14:05:14 +0200

    Major changes, refactored most code

--- spacktrackerreboot.user.js
+++ spacktrackerreboot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SpamtrackerReboot
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Rewrite of the spamtracker project, this userscript will notify you using sound and a notification if a new spam post has been posted in any smoke detector supported rooms
 // @author       Ferrybig
 // @match        *://chat.meta.stackexchange.com/*
@@ -15,96 +15,157 @@ window.Spamtracker = (function(target) {
     'use strict';
 
     var useSound = true;
-    var sound = new Audio('//cdn-chat.sstatic.net/chat/meta2.mp3');
+    var sounds = {
+        metastackexchange: '//cdn-chat.sstatic.net/chat/meta2.mp3',
+        stackexchange: '//cdn-chat.sstatic.net/chat/se.mp3',
+        stackoverflow: '//cdn-chat.sstatic.net/chat/so.mp3',
+        serverfault: '//cdn-chat.sstatic.net/chat/sf.mp3',
+        superuser: '//cdn-chat.sstatic.net/chat/su.mp3',
+        askubuntu: '//cdn-chat.sstatic.net/chat/ubuntu.mp3',
+    };
+    var sound = {
+        default: new Audio(sounds.metastackexchange),
+    };
+    var enabled = true;
+    var metaData = GM_info.script || GM_info.SpamtrackerReboot;
 
     var callback;
     var lastMessageObserverTarget;
     var lastMessageObserver;
+    /**
+    * List of open web notification
+    */
     var notifications = {};
 
+    /**
+    * Loads this userscript
+    */
+    var init = function() {
+        registerObserver();
+        restoreCallback();
+    };
+
+    /**
+    * Restores the callback to the orginal function
+    */
     var restoreCallback = function() {
-        callback = function(url, elm) {
-            if('fire' in window) {
+        callback = function(msg) {
+            if('fire' in window && 'openReportPopupForMessage' in window.fire) {
                 window.focus();
-                fire.openReportPopupForMessage(elm);
+                fire.openReportPopupForMessage(msg.elm);
             } else {
-                window.open(url);
+                window.open(msg.url);
             }
         };
     };
 
     /**
-    * Usefull four our script to interact with clicking on notifications
+    * Useful for other scripts to interact with clicking on notifications
     */
     var setCallback = function(newCallback) {
         callback = newCallback;
     };
 
-    var notifyMe = function (id, title, message, callback, url, elm) {
-        //console.log("Trying notification!", url);
-        var notification = new Notification(title, { body: message, icon: "//i.imgur.com/kS4QNIv.png" });
+    /**
+    * Plays the sound effect
+    */
+    var playSound = function(msg) {
+        if(useSound) {
+            sound.default.play();
+        }
+    };
+
+    /**
+    * Creates a notification for a post
+    */
+    var notifyMe = function (msg) {
+        playSound(msg);
+        var notification = new Notification(msg.title, { body: msg.message, icon: '//i.stack.imgur.com/WyV1l.png?s=128&g=1' });
         notification.onshow = function() {
-            setTimeout(function() {
-                dismissNotification(id);
+            msg.timeout = window.setTimeout(function() {
+                dismissNotification(msg.id);
             }, 15000);
         };
         notification.onclick = function() {
-            callback(url, elm);
-            dismissNotification(id);
+            callback(msg);
+            dismissNotification(msg.id);
         };
-        notifications[id] = notification;
+        notifications[msg.id] = notification;
     };
 
+    /**
+    * Close notification by id
+    */
     var dismissNotification = function(id) {
-        notifications[id].close();
-        delete notifications[id];
+        if(notifications[id]) {
+            notifications[id].close();
+            delete notifications[id];
+        }
     };
 
-    function processChatMessage(message) {
+    /**
+    * Progress a message in chat by element
+    */
+    var processChatMessage = function(message) {
         //console.log("Chat message!" + message.children[1].innerHTML);
-        var smoke = /\/\/goo.gl\/eLDYqh|spam|offensive|abusive/i;
-        var content = message.children[1].innerHTML;
-        var i, msg = {}, parts, ch, path, hash, site = '', qId = '';
-        if (smoke.test(content) && /\/\/[a-z]*.stackexchange.com|stackoverflow.com|superuser.com|serverfault.com|askubuntu.com|stackapps.com|mathoverflow.net/i.test(content)) {
-            //console.log("Match!");
-            ch = message.children[1].children;
-            for (i=ch.length-1; i>=0; i--) {
-                if (ch[i].tagName == 'A') {
-                    hash = ch[i].href.split('#');
-                    path = ch[i].href.split('/');
-                    if (path[3] == 'questions' && hash.length>1) {
-                        site = path[2];
-                        qId = hash[1];
-                    }
-                    else if (/^[qa]/.test(path[3])) {
-                        site = path[2];
-                        qId = path[4];
-                    }
-                }
-            }
-            if (site && qId && useSound) {
-                sound.play();
-            }
-            msg.id = site+'-'+qId+'-'+Date.now();
-            parts = message.children[1].textContent.split(': ');
-            if (parts.length > 1) {
-                msg.title = parts[0];
-                msg.message = parts[1];
-            } else {
-                // msg.title = 'Flag Request';
-                // msg.message = message.children[1].textContent;
-                return; // Not used for now...
-            }
-            msg.type = 'chat';
-            notifyMe(msg.id, msg.title, msg.message, callback, '//' + site + '/q/' + qId, message);
+        if(!message.children[1]) {
+            return false;
         }
-    }
+        var smoke = /\/\/goo.gl\/eLDYqh/i;
+        var sePostRegex = /\/\/[a-z]*.stackexchange.com|stackoverflow.com|superuser.com|serverfault.com|askubuntu.com|stackapps.com|mathoverflow.net/i;
+        var content = message.children[1].innerHTML;
+        var textContent = message.children[1].textContent;
+
+        if (!smoke.test(content) || !sePostRegex.test(content)) {
+            return false;
+        }
+        //console.log("Match!");
+        var ch = message.children[1].children;
+        var msg = {};
+        msg.site = false;
+        msg.qId = false;
+
+        // Loop through all A tags, in search of a link to a stackexchange site, update information in `msg` with the last SE link
+        for (var i = ch.length - 1; i >= 0; i--) {
+            if (ch[i].tagName !== 'A') {
+                continue;
+            }
+            var hash = ch[i].href.split('#');
+            var path = ch[i].href.split('/');
+            if (path[3] === 'questions' && hash.length > 1) {
+                msg.site = path[2];
+                msg.qId = hash[1];
+            } else if (/^[qa]/.test(path[3])) {
+                msg.site = path[2];
+                msg.qId = path[4];
+            }
+        }
+        if (!msg.site || !msg.qId) {
+            return false;
+        }
+        var parts = textContent.indexOf(': ');
+        if (parts < 0) {
+            return false;
+        }
+        var prefixStart = textContent.indexOf('] ');
+        msg.id = message.id;
+        msg.reason = textContent.substring(prefixStart + 2, parts).split(', ');
+        msg.title = "[ SmokeDetector ] \n" + msg.reason.join("\n");
+        msg.message = textContent.substring(parts + 1);
+        msg.url = '//' + msg.site + '/q/' + msg.qId;
+        msg.elm = message;
+        notifyMe(msg);
+        return true;
+    };
+
+    /**
+    * Register an observer on the .messages element
+    */
     var registerMessageObserver = function(elm) {
         if(elm === lastMessageObserverTarget) {
             return;
         }
 
-        //console.log("Register message observer!");
         lastMessageObserverTarget = elm;
         if(lastMessageObserver !== undefined) {
             lastMessageObserver.disconnect();
@@ -119,8 +180,10 @@ window.Spamtracker = (function(target) {
         lastMessageObserver.observe(elm, { childList: true });
     };
 
+    /**
+    * Register an observer on the .monolog.user-container.user-{*}  element
+    */
     var registerMonologObserver = function(elm) {
-        //console.log("Register monolog observer!");
         var children = elm.getElementsByClassName('messages');
         if(children.length) {
             registerMessageObserver(children[children.length - 1]);
@@ -133,8 +196,10 @@ window.Spamtracker = (function(target) {
         }
     };
 
+    /**
+    * Register an observer on the #chat element
+    */
     var registerObserver = function() {
-        //console.log("Register main observer!");
         Notification.requestPermission();
         var children = target.getElementsByClassName('monologue');
         if(children.length) {
@@ -146,15 +211,27 @@ window.Spamtracker = (function(target) {
         observer.observe(target, { childList: true });
     };
 
+    var getConfigOption = function(key, defaultValue) {
+        var data = window.localStorage.getItem(metaData + '-' + key);
+        if(data === undefined) {
+            return defaultValue;
+        }
+        return data;
+    };
 
+    var setConfigOption = function(key, value) {
+        var old = getConfigOption(key, undefined);
+        window.localStorage.setItem(metaData + '-' + key, value);
+        return old;
+    };
 
-    registerObserver();
-    restoreCallback();
-
+    init();
 
     return {
         setCallback: setCallback,
         restoreCallback: restoreCallback,
+        processChatMessage: processChatMessage,
+        metaData: metaData
     };
 
 })(document.getElementById('chat'));
